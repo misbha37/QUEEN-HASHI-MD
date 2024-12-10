b1527045c199621174bf21fe900fc67e5f96db23
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-10 14:14:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG=================================
 SESSION_ID: process.env.SESSION_ID || "Put Session Id",  // ADD Your Session Id 
+MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
