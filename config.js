f94d68bb5417ac4b1e6838236baddbf7a713c263
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-11 13:34:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ module.exports = {
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG=================================
 SESSION_ID: process.env.SESSION_ID || "add season id",  // ADD Your Session Id 
 MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority",
-MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
 BOT_NAME: process.env.BOT_NAME || "nuro",
