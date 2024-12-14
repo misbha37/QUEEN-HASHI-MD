34a80dca26961bc39316d3cea3b1f37e9a62c36e
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-14 19:49:27 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -1,144 +1,92 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
+
 cmd({
     pattern: "song",
-    alias: ["mp3","play"],
-    desc: "To download songs.",
-    react: "🎧",
+    desc: "download songs",
     category: "download",
+    react: "🎵",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*Please give me a url or title*")  
+if(!q) return reply("*කරුණාකර Link එකක් හො නමක් ලබා දෙන්න 🔎...*")
 const search = await yts(q)
-const data = search.videos[0];
+const data = search.videos[0]
 const url = data.url
-    
-    
-let desc = `🎧 *_-QUEEN-ISHU-MD AUDIO DOWNLOADER_* 🎧
 
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-┃► ℹ️ *Title:* ${data.title} 
-┃► 👤 *Duration:* ${data.timestamp} 
-┃► 👁️‍🗨️ *Views:* ${data.views} 
-┃► 🕘 *Uploaded On:* ${data.ago} 
-┃► 📌 *Ago:* ${data.ago}
-┃► 🔗 *Url* ${data.url} 
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+let desc = `*◆ NETHU-MD SONG DOWNLOADER ◆*
 
-*Choose Your Download Format*
+| ➤ TITLE - ${data.title}
 
-*1 Audio File🎧*
-*2 Document File📁*
+| ➤ VIEWS - ${data.views}
 
-*©QUEEN-ISHU*
+| ➤ DESCRIPTION - ${data.description}
+
+| ➤ TIME - ${data.timestamp}
+
+|➤ AGO - ${data.ago}
+
+ ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx
 `
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
-const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+//download audio
 
-        conn.ev.on('messages.upsert', async (msgUpdate) => {
-            const msg = msgUpdate.messages[0];
-            if (!msg.message || !msg.message.extendedTextMessage) return;
+let down = await fg.yta(url)  
+let downloadUrl = down.dl_url
 
-            const selectedOption = msg.message.extendedTextMessage.text.trim();
+//send audio
+await conn.sendMessage(from,{audio:{url: downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"audio/mpeg",fileName:data.title + "mp3",caption:"©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx"},{quoted:mek})
+}catch(e){
+reply(`${e}`)
+}
+})
 
-            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
-                switch (selectedOption) {
-                    case '1':
-                        let down = await fg.yta(url);
-                        let downloadUrl = down.dl_url;
-                        await conn.sendMessage(from, { audio: { url:downloadUrl }, caption: '*©QUEEN-ISHU*', mimetype: 'audio/mpeg'},{ quoted: mek });
-                        break;
-                    case '2':               
-                        // Send Document File
-                        let downdoc = await fg.yta(url);
-                        let downloaddocUrl = downdoc.dl_url;
-                        await conn.sendMessage(from, { document: { url:downloaddocUrl }, caption: '*©QUEEN-ISHU*', mimetype: 'audio/mpeg', fileName:data.title + ".mp3"}, { quoted: mek });
-                        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } })
-                        break;
-                    default:
-                        reply("Invalid option. Please select a valid option🔴");
-                }
-
-            }
-        });
-
-    } catch (e) {
-        console.error(e);
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
-        reply('An error occurred while processing your request.');
-    }
-});
-
-
-//====================video_dl=======================
+//===========video-dl===========
 
 cmd({
     pattern: "video",
-    desc: "To download videos.",
-    react: "🎥",
+    desc: "download video",
     category: "download",
+    react: "🎥",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("*Please give me a url or title*")  
+if(!q) return reply("*කරුණාකර Link එකක් හො නමක් ලබා දෙන්න 🔎...*")
 const search = await yts(q)
-const data = search.videos[0];
+const data = search.videos[0]
 const url = data.url
-    
-    
-let desc = `📽️ *_QUEEN-ISHU-MD VIDEO DOWNLOADER_* 📽️
 
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-┃► ℹ️ *Title:* ${data.title} 
-┃► 👤 *Duration:* ${data.timestamp} 
-┃► 👁️‍🗨️ *Views:* ${data.views} 
-┃► 🕘 *Uploaded On:* ${data.ago} 
-┃► 📌 *Ago:* ${data.ago}
-┃► 🔗 *Url* ${data.url} 
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+let des = `*◆ NETHU-MD VIDEO DOWNLOADER ◆*
 
-*Choose Your Download Format*
+| ➤ TITLE - ${data.title}
 
-*1 Video File📽️*
-*2 Document File📁*
+| ➤ VIEWS - ${data.views}
 
-*©QUEEN-ISHU*
+| ➤ DESCRIPTION - ${data.description}
+
+| ➤ TIME - ${data.timestamp}
+
+| ➤ AGO - ${data.ago}
+
+©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx
 `
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:des},{quoted:mek});
 
- const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+//download video
 
-        conn.ev.on('messages.upsert', async (msgUpdate) => {
-            const msg = msgUpdate.messages[0];
-            if (!msg.message || !msg.message.extendedTextMessage) return;
+let down = await fg.ytv(url)  
+let downloadUrl = down.dl_url
 
-            const selectedOption = msg.message.extendedTextMessage.text.trim();
-
-            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
-                switch (selectedOption) {
-                    case '1':
-                        let downvid = await fg.ytv(url);
-                        let downloadvUrl = downvid.dl_url;
-                        await conn.sendMessage(from, { video : { url:downloadvUrl }, caption: '*©QUEEN-ISHU*', mimetype: 'video/mp4'},{ quoted: mek });
-                        break;
-                    case '2':
-                        let downviddoc = await fg.ytv(url);
-                        let downloadvdocUrl = downviddoc.dl_url;
-                        await conn.sendMessage(from, { document: { url:downloadvdocUrl }, caption: '*©QUEEN-ISHU*', mimetype: 'video/mp4', fileName:data.title + ".mp4" }, { quoted: mek });
-                        break;
-                    default:
-                        reply("Invalid option. Please select a valid option🔴");
-                }
-
-            }
-        });
-
-    } catch (e) {
-        console.error(e);
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
-        reply('An error occurred while processing your request.');
-    }
-});
+//send video
+await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx"},{quoted:mek})
+    
+}catch(a){
+reply(`${a}`)
+}
+})
