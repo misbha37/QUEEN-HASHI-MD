a2a1560b30375a6682f33dd0b020d623eeaca72d
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-14 15:06:41 +0530

    舞~   🎀  𝒬𝒰𝐸𝐸𝒩 𝐼𝒮𝐻𝒰  🎀   ~舞

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG===============================
 SESSION_ID: process.env.SESSION_ID || "add season id",  // ADD Your Session Id 
-MONGODB: process.env.MONGODB || "add mongodb url",
+MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority",
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
 BOT_NAME: process.env.BOT_NAME || "queen-ishu",
