8486ec7e07cde4d647a99a7d063f4699fafebeac
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-18 18:49:04 +0530

    Update mega.js

--- plugins/mega.js
+++ plugins/mega.js
@@ -24,26 +24,26 @@ cmd({
         const data = await file.downloadBuffer();
         
         if (/mp4/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "video/mp4", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "video/mp4", filename: ${file.name} }, { quoted: mek }');
         } else if (/pdf/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "application/pdf", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "application/pdf", filename: ${file.name} }, { quoted: mek }');
         } else if (/zip/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "application/zip", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "application/zip", filename: ${file.name} }, { quoted: mek }');
         } else if (/rar/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "application/x-rar-compressed", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage(from, '{ document: data, mimetype: "application/x-rar-compressed", filename: ${file.name} }, { quoted: mek }');
         } else if (/7z/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "application/x-7z-compressed", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "application/x-7z-compressed", filename: ${file.name} }, { quoted: mek }');
         } else if (/jpg|jpeg/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "image/jpeg", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "image/jpeg", filename: ${file.name} }, { quoted: mek }');
         } else if (/png/.test(file.name)) {
-            await conn.sendMessage(from, { document: data, mimetype: "image/png", filename: ${file.name} }, { quoted: mek });
+            await conn.sendMessage('from, { document: data, mimetype: "image/png", filename: ${file.name} }, { quoted: mek }');
         } else {
-            await conn.sendMessage(from, { document: data, mimetype: "application/octet-stream", filename: ${file.name} }, { quoted: mek })
+            await conn.sendMessage('from, { document: data, mimetype: "application/octet-stream", filename: ${file.name} }, { quoted: mek }')
         }
         
         await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
     } catch (e) {
         console.log(e)
-        reply(${e})
+        reply('${e}')
     }
 });
