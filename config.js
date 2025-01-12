6395d480c1909d29d93f373a0d2261b832db31dd
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2025-01-12 12:44:46 +0530

    ꧁༒☠💥✨⚜️𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐁𝐲 : ® 𝐌𝐑 𝐋𝐀𝐊𝐒𝐈𝐃𝐔✨💥☠༒꧂

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG===============================
+///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG====================
 SESSION_ID: process.env.SESSION_ID || "add season id",  // ADD Your Session Id 
 //===========================මෙතනට-mongodburl-එකක් ඇඩ්කරගන්න============================================
 MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority",
@@ -17,9 +17,6 @@ BOT_NAME: process.env.BOT_NAME || "queen-ishu",
 GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!ALIVE-CONFIG===============================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/vzDwkjg.jpeg", //add alive img url
-ALIVE_MSG: process.env.ALIVE_MSG || "💜✅ HI I AM WHATSAPP BOT ✅💜", //add alive msg
 //=========================මේ උඩ තියන දෙක වෙනස් කරන්න එපා වෙනස් කරත් වැඩක් නෑ=============
 ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
 ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
